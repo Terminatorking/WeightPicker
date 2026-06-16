8675e6f7368719bc26ee84e1770ee9259fa73e01
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2026-06-16 15:59:28 +0330

    handle system bar colors and delete unnecessary components

--- app/src/main/java/ghazimoradi/soheil/weightpicker/ui/activity/MainActivity.kt
+++ app/src/main/java/ghazimoradi/soheil/weightpicker/ui/activity/MainActivity.kt
@@ -2,46 +2,30 @@ package ghazimoradi.soheil.weightpicker.ui.activity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle.Companion.dark
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import ghazimoradi.soheil.weightpicker.ui.theme.Gray
 import ghazimoradi.soheil.weightpicker.ui.theme.WeightPickerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        enableEdgeToEdge(
+            statusBarStyle = dark(Gray),
+            navigationBarStyle = dark(Gray)
+        )
+
         setContent {
             WeightPickerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WeightPickerTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/ghazimoradi/soheil/weightpicker/ui/theme/Theme.kt
+++ app/src/main/java/ghazimoradi/soheil/weightpicker/ui/theme/Theme.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -23,6 +24,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val Gray = Color.Gray.toArgb()
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
